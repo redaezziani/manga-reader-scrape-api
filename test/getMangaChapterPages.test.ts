30c4bec22fe5b94cc2a128481209a9f991ca32b0
redaezziani <klausdev2@gmail.com>
2024-08-27 22:52:29 +0100

    feat: Implement endpoint to retrieve manga chapter pages

--- test/getMangaChapterPages.test.ts
+++ test/getMangaChapterPages.test.ts
@@ -3,9 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 describe('GET /api/v1/manga/chapter', () => {
-    it('should return manga chapter pages when valid title and chapter are provided', async () => {
-        ///jujutsu-kaisen/266/
-    
+    it('should return manga chapter pages when valid title and chapter are provided', async () => {    
         const response = await fetch(`http://localhost:${process.env.PORT}/api/v1/manga/chapter?title=jujutsu-kaisen&chapter=266`,
         {
             method: 'GET',
