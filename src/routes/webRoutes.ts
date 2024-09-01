675725ed5f1c5b2c6e9a4be2ac31731482980ea7
redaezziani <klausdev2@gmail.com>
2024-09-01 18:56:17 +0100

    feat: Add home page

--- src/routes/webRoutes.ts
+++ src/routes/webRoutes.ts
@@ -7,5 +7,16 @@ webRouter.get('/', async (req, res) => {
     res.render('index');
 });
 
+webRouter.get('/home', async (req, res) => {
+    try {
+        const mangaList = await fetch('http://localhost:8080/api/v1/manga/latest?source=Trend');
+        const data = await mangaList.json();
+        res.render('home', { data: data.data });
+    } catch (error) {
+        console.error(error);
+        res.render('error');
+    }
+});
+
 
 export default webRouter;
